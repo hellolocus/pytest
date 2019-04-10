c46f085c9c69041601dd51640b4be68f8b5e3241
mockdate <469795236@qq.com>
2019-04-10 17:05:05 +0800

    pytest main函数

--- src/pytestdemo.py
+++ src/pytestdemo.py
@@ -1,2 +1,11 @@
 #!/usr/bin/env python
+# 主函数使用方法
 import pytest
+def test_a():
+    print('----begin test a')
+
+def test_b():
+    print('----begin test b')
+
+if __name__ == '__main__':
+    pytest.main("-s pytestdemo.py")
